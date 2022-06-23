836e0870873994c508dc24a2a00ca8f728ba1f49
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-23 17:52:56 +0530

    [Tushar Vidwans] Adding welcome message for this program

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static  void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cabInvoice Generator");
+
+            
+        }
+    }
+}
