f63dd40c70800b94296e8c47da7fbaefc39d165a
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-23 21:21:28 +0530

    [Tushar Vidwans] [Test case 3 ]- Givens the multiple rides should return invoice summary with average fare

--- CabInvoice_TDD/Tests.cs
+++ CabInvoice_TDD/Tests.cs
@@ -44,5 +44,26 @@ namespace CabInvoice_TDD
             //Asserting values
             Assert.AreEqual(expectedSummary, summary);
         }
+
+
+        /// <summary>
+        /// Givens the multiple rides should return invoice summary with average fare.  UC3
+        /// </summary>
+        [TestMethod]
+        public void GivenMultipleRidesShouldReturnInvoiceSummarywithAverageFare()
+        {
+            //Arrange
+            //Creating instance of invoice generator 
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            //Act
+            //Generating Summary for rides
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+
+            //Asserting values with average in equals to formula in invoice summary
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
