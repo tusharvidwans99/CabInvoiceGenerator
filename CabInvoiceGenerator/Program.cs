9ec716603abf7cae1ed105c53e7ae885ceb4c9a4
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-23 18:04:09 +0530

    [Tushar Vidwans] Added: Fare Calculator, multiple rides, Enhanced Invoice, Invoice service, Premium rides

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,11 @@
         {
             Console.WriteLine("Welcome to cabInvoice Generator");
 
-            
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+
+            Console.WriteLine(fare);
         }
     }
 }
